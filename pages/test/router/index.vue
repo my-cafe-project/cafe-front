0a74a98750695357cb34acc4d73370aede6a82fd
sudong <sudong@mobidoc.us>
2024-01-02 23:54:21 +0900

    [main] router test page

--- pages/test/router/index.vue
+++ pages/test/router/index.vue
@@ -10,8 +10,12 @@
 <script setup lang="ts">
 const { $router } = useNuxtApp();
 
+/**
+ * $router.push를 원래 사용했으나, nuxt에서는 navigateTo를 지원해서 이걸 사용해도 됩니다.
+ */
 function onClickRouterTest() {
-  $router.push('/test/router/historyback')
+  // $router.push('/test/router/historyback');
+  navigateTo('/test/router/historyback');
 }
 </script>
 <style scoped lang="scss">
